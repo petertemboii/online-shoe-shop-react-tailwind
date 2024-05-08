c209b3b380793fc7fe1dd60bae0d4fae55ee7399
petertemboii <tembopeter90@gmail.com>
2024-05-08 14:49:43 +0200

    Fix the line height issue on small devices.

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -16,7 +16,8 @@ const Hero = () => {
         <p className="text-xl font-montserrat text-coral-red">
           Our Summer Collection
         </p>
-        <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82] font-bold">
+        {/* Removed the line height css class: max-sm:leading-[82] */}
+        <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] font-bold">
           <span className="xl:bg-white xl:whitespace-nowrap relative z10 pr-10">
             The New Arrival
           </span>
